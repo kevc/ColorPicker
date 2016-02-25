2ee853bae9696d062af174e129e1f2ecc3986386
Kevin Carpenter <kevinjcarpenter@gmail.com>
2016-02-24 20:14:44 -0500

    Implement viewmodel in ColorPickerView

---
+++ app/src/main/java/kevcar/me/colorpicker/view/ColorPickerView.java
@@ -0,0 +1,83 @@
+package kevcar.me.colorpicker.view;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+
+import com.jakewharton.rxbinding.widget.RxSeekBar;
+
+import kevcar.me.colorpicker.R;
+import me.kevcar.model.RGB;
+import me.kevcar.viewmodel.ColorPickerViewModel;
+import rx.functions.Action1;
+import rx.subscriptions.CompositeSubscription;
+
+public class ColorPickerView extends RelativeLayout {
+
+    // ViewModel
+
+    private ColorPickerViewModel viewModel = new ColorPickerViewModel();
+
+    // Views
+
+    private ImageView target;
+    private SeekBar red;
+    private SeekBar green;
+    private SeekBar blue;
+
+    // Subscriptions
+
+    private CompositeSubscription subscriptions = new CompositeSubscription();
+
+    // Constructor
+
+    public ColorPickerView(Context context) {
+        super(context);
+    }
+
+    public ColorPickerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    // Lifecycle
+
+    @Override
+    public void onFinishInflate() {
+        super.onFinishInflate();
+        target = (ImageView) findViewById(R.id.target);
+        red = (SeekBar) findViewById(R.id.red);
+        green = (SeekBar) findViewById(R.id.green);
+        blue = (SeekBar) findViewById(R.id.blue);
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        // Sources
+        subscriptions.add(RxSeekBar.changes(red).subscribe(viewModel.redSubscriber));
+        subscriptions.add(RxSeekBar.changes(green).subscribe(viewModel.greenSubscriber));
+        subscriptions.add(RxSeekBar.changes(blue).subscribe(viewModel.blueSubscriber));
+
+        // Sinks
+        subscriptions.add(viewModel.rgbSubject.subscribe(rgbOnNext));
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        subscriptions.clear();
+    }
+
+    // Sink implementations
+
+    private Action1<RGB> rgbOnNext = new Action1<RGB>() {
+        @Override
+        public void call(RGB rgb) {
+            int color = Color.rgb(rgb.getRed(), rgb.getGreen(), rgb.getBlue());
+            target.setBackgroundColor(color);
+        }
+    };
+}
